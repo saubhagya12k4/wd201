403a17045d5ef685eb1e60793b835b2099215765
Saubhagya Vishwakarma <126088603+saubhagya12k4@users.noreply.github.com>
2023-03-22 22:17:22 +0530

    Update todo.js

--- todo-cli/models/todo.js
+++ todo-cli/models/todo.js
@@ -1,7 +1,7 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+// models/todo.js
+"use strict";
+const { Op } = require("sequelize");
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Todo extends Model {
     /**
@@ -9,68 +9,67 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static async associate(models) {
+    static async addTask(params) {
       return await Todo.create(params);
     }
     static async showList() {
       console.log("My Todo list \n");
 
       console.log("Overdue");
-      console.log(
-        (await Todo.overdue())
-          .map((todo) => {
-            return todo.displayableString();
-          })
-          .join("\n")
-      );
+      const overdueItems = await this.overdue();
+      const overdueList = overdueItems.map((todo) => todo.displayableString());
+      console.log(overdueList.join("\n").trim());
+
       console.log("\n");
 
       console.log("Due Today");
-      // FILL IN HERE
-      console.log(
-        (await Todo.dueToday())
-          .map((todo) => todo.displayableString())
-          .join("\n")
-      );
+      const todayList = await this.dueToday();
+      const todayItems = todayList.map((todo) => todo.displayableString());
+      console.log(todayItems.join("\n").trim());
       console.log("\n");
 
       console.log("Due Later");
-      console.log(
-        (await Todo.dueLater())
-          .map((todo) => todo.displayableString())
-          .join("\n")
+      const dueLaterList = await this.dueLater();
+      const dueLaterItems = dueLaterList.map((todo) =>
+        todo.displayableString()
       );
+      console.log(dueLaterItems.join("\n").trim());
     }
 
     static async overdue() {
-      // FILL IN HERE TO RETURN OVERDUE ITEMS
       return await Todo.findAll({
         where: {
-          dueDate: { [Op.lt]: new Date().toLocaleDateString("en-CA") },
+          dueDate: {
+            [Op.lt]: new Date(),
+          },
         },
+        order: [["id", "ASC"]],
       });
     }
 
     static async dueToday() {
-      // FILL IN HERE TO RETURN ITEMS DUE tODAY
       return await Todo.findAll({
         where: {
-          dueDate: { [Op.eq]: new Date().toLocaleDateString("en-CA") },
+          dueDate: {
+            [Op.eq]: new Date(),
+          },
         },
+        order: [["id", "ASC"]],
       });
     }
 
     static async dueLater() {
-      // FILL IN HERE TO RETURN ITEMS DUE LATER
       return await Todo.findAll({
         where: {
-          dueDate: { [Op.gt]: new Date().toLocaleDateString("en-CA") },
+          dueDate: {
+            [Op.gt]: new Date(),
+          },
         },
+        order: [["id", "ASC"]],
       });
     }
 
     static async markAsComplete(id) {
-      // FILL IN HERE TO MARK AN ITEM AS COMPLETE
       await Todo.update(
         { completed: true },
         {
@@ -83,20 +82,22 @@ module.exports = (sequelize, DataTypes) => {
 
     displayableString() {
       let checkbox = this.completed ? "[x]" : "[ ]";
-      return `${this.id}. ${checkbox} ${this.title} ${
-        this.dueDate == new Date().toLocaleDateString("en-CA")
-          ? ""
-          : this.dueDate
-      }`.trim();
+      const day = new Date(this.dueDate);
+      return day.getDate() === new Date().getDate()
+        ? `${this.id}. ${checkbox} ${this.title}`.trim()
+        : `${this.id}. ${checkbox} ${this.title} ${this.dueDate}`.trim();
     }
   }
-  Todo.init({
-    title: DataTypes.STRING,
-    dueDate: DataTypes.DATEONLY,
-    completed: DataTypes.BOOLEAN
-  }, {
-    sequelize,
-    modelName: 'Todo',
-  });
+  Todo.init(
+    {
+      title: DataTypes.STRING,
+      dueDate: DataTypes.DATEONLY,
+      completed: DataTypes.BOOLEAN,
+    },
+    {
+      sequelize,
+      modelName: "Todo",
+    }
+  );
   return Todo;
 };
